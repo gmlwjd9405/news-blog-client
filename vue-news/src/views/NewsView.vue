990b433856d0078cdc1be8190e41c7aaa30d57ed
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-07 12:38:59 +0900

    Add axios and Call api using axios

---
+++ vue-news/src/views/NewsView.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div v-for="user in users">{{ user.title }}</div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    /* callback의 this 바인딩을 View Component를 바라보도록! (화살표 함수는 없어도 됨) */
+    // var vm = this; 
+
+    // axios.get('http://localhost:8080/api/users')
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(response => {
+        this.users = response.data;
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  },
+}
+</script>
+
+<style>
+
+</style>
