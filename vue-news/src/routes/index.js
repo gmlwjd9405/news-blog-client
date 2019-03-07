1772b8d879cf5762eb84480013b6c4939e6ffb24
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-07 11:31:00 +0900

    Change dir structure and Delete deault setting # tag in url

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -4,10 +4,13 @@ import VueRouter from "vue-router";
 import NewsView from "../views/NewsView.vue";
 import AskView from "../views/AskView.vue";
 import JobsView from "../views/JobsView.vue";
+import ItemView from "../views/ItemView.vue";
+import UserView from "../views/UserView.vue";
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+  mode: 'history',
   // router instance 생성
   routes: [
     {
@@ -25,6 +28,14 @@ export const router = new VueRouter({
     {
       path: "/jobs",
       component: JobsView
+    },
+    {
+      path: "/item",
+      component: ItemView
+    },
+    {
+      path: "/users",
+      component: UserView
     }
   ]
 });
---
+++ vue-news/src/views/ItemView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Item</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
