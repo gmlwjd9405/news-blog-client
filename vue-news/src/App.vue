5e2581e02705ffad960a35aca3b2d223f5b8fe48
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-08 12:39:38 +0900

    Apply router transition for changing page

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -2,23 +2,33 @@
   <div id="app">
     <ToolBar></ToolBar>
     <!-- url 주소에 따라서 지정한 Component가 뿌려짐 -->
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import ToolBar from './components/ToolBar.vue'
+import ToolBar from "./components/ToolBar.vue";
 
 export default {
   components: {
-    ToolBar,
-  },
-}
+    ToolBar
+  }
+};
 </script>
 
 <style>
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active, .page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
