9038b3764306bf1f415f94efcf42c0fde5b4c216
Kwon HeeJeong <gmlwjd9405@naver.com>
2019-03-08 13:16:06 +0900

    Add styling newslist

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -24,6 +24,16 @@ body {
   margin: 0;
 }
 
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+
 /* Router Transition */
 .page-enter-active, .page-leave-active {
   transition: opacity 0.5s;
